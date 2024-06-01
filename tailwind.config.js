b67f9fff73c31e8e987f0b1ec445097125ccd329
valyastriz <valyastriz@gmail.com>
2024-06-01 19:58:55 -0400

    added tailwind links to begin using tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html",    // Include all HTML files in src directory and its subdirectories
+    "./src/**/*.js",      // Include all JS files in src directory and its subdirectories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
