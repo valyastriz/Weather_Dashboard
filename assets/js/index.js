68477fa6ec400acf8d2df598b5ced06f87df6272
valyastriz <valyastriz@gmail.com>
2024-06-07 21:00:28 -0400

    finished the search function, started fiveDay function

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,41 @@ const cityEl = document.getElementById('cityInput');
 function handleSubmit(event) {
     event.preventDefault();
     const city = document.getElementById('city').value;
-    console.log(city);
+    search(city);
 }
+
+function search(city) {
+    if(!city || city.length < 2) {
+        errorBox.textContent = "Please enter a valid city name and try again."
+        return;
+    }
+    const apiKey = '1d06b7c740fb5f44ff9ef2d948306601';
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+    fetch(url)
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        fiveDay(lat, lon);
+    })
+    .catch(function(error) {
+        console.error("Error: ", error)
+    });
+}
+
+function fiveDay(lat, lon) {
+    const APIKey = '1d06b7c740fb5f44ff9ef2d948306601';
+    const url = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${url}`
+    fetch(url)
+        .then(function(data) {
+            return resp.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+    }
 
 //listener for the 'submit' on the city search
 cityEl.addEventListener('submit', handleSubmit)
