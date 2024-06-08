aded76d5ec7471833343db988e3e12140de37059
valyastriz <valyastriz@gmail.com>
2024-06-07 20:19:17 -0400

    starter function for handle submit created

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const cityEl = document.getElementById('cityInput');
+
+//handled the submit of the city search
+function handleSubmit(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    console.log(city);
+}
+
+
+//listener for the 'submit' on the city search
+cityEl.addEventListener('submit', handleSubmit)
