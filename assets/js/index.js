cb66faa9de80c07c60042fdb38bd025b38b37f18
valyastriz <valyastriz@gmail.com>
2024-06-07 21:36:19 -0400

    added formatDate function to convert date to m/dd/yyyy format

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,18 +41,32 @@ function fiveDay(lat, lon) {
             data.list.forEach((item, index) => {
                 if (index % 8 === 0) { // if the index number is exactly divisible by 8 (there are 8 indices for each day so we only need one, then take the info from that index)
                     const tempF = (item.main.temp - 273.15) * 9/5 + 32; // converting from kelvin to farenhight
+                    // newDate = item.dt.text
                     fiveDayArr.push({
-                        data: item.dt_txt,
+                        date: formatDate(item.dt_txt),
                         emoji: item.weather[0].icon,
                         temp: tempF.toFixed(1), //rounds to one decimal place
                         wind: item.wind.speed,
                         humidity: item.main.humidity,
                     });
-                    console.log(fiveDayArr);
                 }
             });
+            console.log(fiveDayArr);
         });
     }
+
+function formatDate(dateString) {
+    //create date object from date input string
+    const date = new Date(dateString);
+    //extract the date from the date object
+    const day = date.getDate().toString().padStart(2, '0');
+    //extract the month from the date object
+    const month = (date.getMonth() + 1).toString();
+    //extract the year from the date object
+    const year = date.getFullYear();
+    //return formatedd date string
+    return `${month}/${day}/${year}`;
+}
 
 //listener for the 'submit' on the city search
 cityEl.addEventListener('submit', handleSubmit)
