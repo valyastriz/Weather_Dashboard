7b64a41cfb518f7abef3585af83167ec2340296d
valyastriz <valyastriz@gmail.com>
2024-06-08 02:45:54 -0400

    trying to fix display/rendering issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,158 +1,171 @@
-const cityEl = document.getElementById('cityInput');
-let fiveDayArr = JSON.parse(localStorage.getItem('fiveDayArr')) || []; //load from local storage or initialize an empty array
+document.addEventListener('DOMContentLoaded', () => {
 
-//handled the submit of the city search
-function handleSubmit(event) {
-    event.preventDefault();
-    const city = document.getElementById('city').value;
-    search(city);
-    document.getElementById('city').value = '';
-}
+    const cityEl = document.getElementById('cityInput');
+    let fiveDayArr = JSON.parse(localStorage.getItem('fiveDayArr')) || []; //load from local storage or initialize an empty array
 
-function search(city) {
-    if(!city || city.length < 2) {
-        errorBox.textContent = "Please enter a valid city name and try again."
-        return;
+    //handled the submit of the city search
+    function handleSubmit(event) {
+        event.preventDefault();
+        const city = document.getElementById('city').value;
+        search(city);
+        document.getElementById('city').value = '';
     }
-    const apiKey = '1d06b7c740fb5f44ff9ef2d948306601';
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
-    fetch(url)
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        const lat = data[0].lat;
-        const lon = data[0].lon;
-        fiveDay(lat, lon, city);
-    })
-    .catch(function(error) {
-        console.error("Error: ", error)
-    });
-}
 
-function fiveDay(lat, lon, city) {
-    const apiKey = '1d06b7c740fb5f44ff9ef2d948306601';
-    const urlFive = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    const urlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    
-    fetch(urlCurrent)
+    function search(city) {
+        if(!city || city.length < 2) {
+            errorBox.textContent = "Please enter a valid city name and try again."
+            return;
+        }
+        const apiKey = '1d06b7c740fb5f44ff9ef2d948306601';
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+        fetch(url)
         .then(function(resp) {
             return resp.json();
         })
-        .then(function(currentData) {            
-            const currentTempF = (currentData.main.temp - 273.15) * 9/5 + 32; // Convert from Kelvin to Fahrenheit
-            const currentIconCode = currentData.weather[0].icon; // Get icon code
-            const currentIconUrl = `http://openweathermap.org/img/wn/${currentIconCode}@2x.png`; // Construct icon URL
-
-            let cityData = [{
-                city: city,
-                date: formatDate(currentData.dt * 1000), // Convert Unix timestamp to date
-                iconUrl: currentIconUrl,
-                temp: currentTempF.toFixed(1), // Round to one decimal place
-                wind: currentData.wind.speed,
-                humidity: currentData.main.humidity
-            }];
-
-            createCurrentCard(cityData, city);
-
-            return fetch(urlFive)
-                .then(function(fiveDayDataResp) {
-                    return fiveDayDataResp.json();
-                })
-                .then(function(fiveDayData) {
-                    fiveDayData.list.forEach((item, index) => {
-                        if (index % 8 === 0) { // Every 8th item
-                            const tempF = (item.main.temp - 273.15) * 9/5 + 32; // Convert Kelvin to Fahrenheit
-                            const iconCode = item.weather[0].icon; // Get icon code
-                            const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`; // Construct icon URL
-
-                            cityData.push({
-                                city: city,
-                                date: formatDate(item.dt_txt),
-                                iconUrl: iconUrl,
-                                temp: tempF.toFixed(1), // Round to one decimal place
-                                wind: item.wind.speed,
-                                humidity: item.main.humidity
-                            });
-                        }
-                    });
-
-                    // Add new data to the beginning of the array using unshift
-                    fiveDayArr.unshift(cityData);
-
-                    // Ensure the array only stores the 5 latest searches
-                    // if (fiveDayArr.length > 5) {
-                    //     fiveDayArr = fiveDayArr.slice(0, 5);
-                    // }
-
-                    // Save to local storage
-                    localStorage.setItem('fiveDayArr', JSON.stringify(fiveDayArr));
-                    console.log(fiveDayArr);
-
-                    createFutureCards();
-                });
+        .then(function(data) {
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            fiveDay(lat, lon, city);
         })
         .catch(function(error) {
-            console.error("Error: ", error);
+            console.error("Error: ", error)
         });
-}
-
-
-function formatDate(dateString) {
-    //create date object from date input string
-    const date = new Date(dateString);
-    //extract the date from the date object
-    const day = date.getDate().toString().padStart(2, '0');
-    //extract the month from the date object
-    const month = (date.getMonth() + 1).toString();
-    //extract the year from the date object
-    const year = date.getFullYear();
-    //return formatedd date string
-    return `${month}/${day}/${year}`;
-}
-
-
-function createFutureCards() {
-    const futureContainerEl = document.getElementById('futureContainer');
-    futureContainerEl.innerHTML = ''; //clears any previous content
-
-    const cityDataArray = fiveDayArr[0] || [];
-
-
-    cityDataArray.forEach(function(item) {
-        const div = document.createElement('div');
-        div.classList.add('col-span-2', 'p-2', 'bg-cyan-600', 'rounded-md', 'shadow-md', 'shadow-cyan-500/50', 'mb-2');
-        div.innerHTML = `
-            <h3 class="font-bold mb-2">${item.date}</h3>
-            <img src="${item.iconUrl}" alt="Weather Icon" class="mb-1.5 w-12 h-12">
-            <p class="mb-1.5">Temp: ${item.temp} °F</p>
-            <p class="mb-1.5">Wind: ${item.wind} MPH</p>
-            <p class="mb-1.5">Humidity: ${item.humidity}%</p>
-        `;
-        futureContainerEl.appendChild(div);
-    });
-}
-
-function createCurrentCard(cityDataArray, city) {
-    const currentCardEl = document.getElementById('currentCard');
-    currentCardEl.innerHTML = ''; //clears any previous content
-
-    if(cityDataArray.length > 0) {
-        const item = cityDataArray[0];
-        const currDiv = document.createElement('div');
-        currDiv.classList.add('col-span-10', 'border-solid', 'border-2', 'border-cyan-800', 'mt-3', 'rounded-md', 'm-2', 'p-2', 'bg-cyan-800', 'text-gray-200', 'shadow-md', 'shadow-cyan-500/50');
-        currDiv.innerHTML = `
-        <h2 class="text-lg font-bold mb-2">${city} ${item.date} <img src="${item.iconUrl}" alt="Weather Icon" class="mb-1.5 w-12 h-12"></h2>
-        <p class="mb-1">Temp: ${item.temp} °F</p>
-        <p class="mb-1">Wind: ${item.wind} MPH</p>
-        <p class="mb-1">Humidity: ${item.humidity}%</p>
-        `;
-        currentCardEl.appendChild(currDiv);
     }
-}
+
+    function fiveDay(lat, lon, city) {
+        const apiKey = '1d06b7c740fb5f44ff9ef2d948306601';
+        const urlFive = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        const urlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        
+        fetch(urlCurrent)
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(function(currentData) {            
+                const currentTempF = (currentData.main.temp - 273.15) * 9/5 + 32; // Convert from Kelvin to Fahrenheit
+                const currentIconCode = currentData.weather[0].icon; // Get icon code
+                const currentIconUrl = `http://openweathermap.org/img/wn/${currentIconCode}@2x.png`; // Construct icon URL
+
+                let cityData = [{
+                    city: city,
+                    date: formatDate(currentData.dt * 1000), // Convert Unix timestamp to date
+                    iconUrl: currentIconUrl,
+                    temp: currentTempF.toFixed(1), // Round to one decimal place
+                    wind: currentData.wind.speed,
+                    humidity: currentData.main.humidity
+                }];
+
+                createCurrentCard(cityData, city);
+
+                return fetch(urlFive)
+                    .then(function(fiveDayDataResp) {
+                        return fiveDayDataResp.json();
+                    })
+                    .then(function(fiveDayData) {
+                        fiveDayData.list.forEach((item, index) => {
+                            if (index % 8 === 0) { // Every 8th item
+                                const tempF = (item.main.temp - 273.15) * 9/5 + 32; // Convert Kelvin to Fahrenheit
+                                const iconCode = item.weather[0].icon; // Get icon code
+                                const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`; // Construct icon URL
+
+                                cityData.push({
+                                    city: city,
+                                    date: formatDate(item.dt_txt),
+                                    iconUrl: iconUrl,
+                                    temp: tempF.toFixed(1), // Round to one decimal place
+                                    wind: item.wind.speed,
+                                    humidity: item.main.humidity
+                                });
+                            }
+                        });
+
+                        // Add new data to the beginning of the array using unshift
+                        fiveDayArr.unshift(cityData);
+
+                        // Ensure the array only stores the 5 latest searches
+                        // if (fiveDayArr.length > 5) {
+                        //     fiveDayArr = fiveDayArr.slice(0, 5);
+                        // }
+
+                        // Save to local storage
+                        localStorage.setItem('fiveDayArr', JSON.stringify(fiveDayArr));
+                        console.log(fiveDayArr);
+
+                        createFutureCards();
+                    });
+            })
+            .catch(function(error) {
+                console.error("Error: ", error);
+            });
+    }
 
 
-//listener for the 'submit' on the city search
-cityEl.addEventListener('submit', handleSubmit)
+    function formatDate(dateString) {
+        //create date object from date input string
+        const date = new Date(dateString);
+        //extract the date from the date object
+        const day = date.getDate().toString().padStart(2, '0');
+        //extract the month from the date object
+        const month = (date.getMonth() + 1).toString();
+        //extract the year from the date object
+        const year = date.getFullYear();
+        //return formatedd date string
+        return `${month}/${day}/${year}`;
+    }
 
-// document.addEventListener('DOMContentLoaded', renderSavedData);
+
+    function createFutureCards() {
+        const futureContainerEl = document.getElementById('futureContainer');
+        // futureCardEl.innerHTML = ''; //clears any previous content
+        console.log(futureContainerEl);
+
+        const cityDataArray = fiveDayArr[0] || [];
+
+
+        cityDataArray.forEach(function(item) {
+            const div = document.createElement('div');
+            div.classList.add('col-span-2', 'p-2', 'bg-cyan-600', 'rounded-md', 'shadow-md', 'shadow-cyan-500/50', 'mb-2');
+            div.innerHTML = `
+                <h3 class="font-bold mb-2">${item.date}</h3>
+                <img src="${item.iconUrl}" alt="Weather Icon" class="mb-1.5 w-12 h-12">
+                <p class="mb-1.5">Temp: ${item.temp} °F</p>
+                <p class="mb-1.5">Wind: ${item.wind} MPH</p>
+                <p class="mb-1.5">Humidity: ${item.humidity}%</p>
+            `;
+            futureContainerEl.appendChild(div);
+        });
+    }
+
+    function createCurrentCard(cityDataArray, city) {
+        const currentCardEl = document.getElementById('currentCard');
+        currentCardEl.innerHTML = ''; //clears any previous content
+
+        if(cityDataArray.length > 0) {
+            const item = cityDataArray[0];
+            const currDiv = document.createElement('div');
+            currDiv.classList.add('col-span-10', 'border-solid', 'border-2', 'border-cyan-800', 'mt-3', 'rounded-md', 'm-2', 'p-2', 'bg-cyan-800', 'text-gray-200', 'shadow-md', 'shadow-cyan-500/50');
+            currDiv.innerHTML = `
+            <h2 class="text-lg font-bold mb-2">${city} ${item.date} <img src="${item.iconUrl}" alt="Weather Icon" class="mb-1.5 w-12 h-12"></h2>
+            <p class="mb-1">Temp: ${item.temp} °F</p>
+            <p class="mb-1">Wind: ${item.wind} MPH</p>
+            <p class="mb-1">Humidity: ${item.humidity}%</p>
+            `;
+            currentCardEl.prepend(currDiv);
+        }
+    }
+
+
+    //listener for the 'submit' on the city search
+    cityEl.addEventListener('submit', handleSubmit)
+
+    function renderSavedData() {
+        if(fiveDayArr.length > 0) {
+            const latestCityData = fiveDayArr[0];
+            createCurrentCard(latestCityData, latestCityData[0].city);
+            createFutureCards();
+        }
+    }
+
+    renderSavedData();
+
+});
