6b67fe7c91517a18cd96497f09b8f11ed99c17d0
valyastriz <valyastriz@gmail.com>
2024-06-08 12:55:33 -0400

    added randomID to the cityData objects so I can access them later

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const cityEl = document.getElementById('cityInput');
     let fiveDayArr = JSON.parse(localStorage.getItem('fiveDayArr')) || []; //load from local storage or initialize an empty array
+    const cityClicked = document.getElementById('cityClicked');
+
+
 
     //handled the submit of the city search
     function handleSubmit(event) {
@@ -35,36 +38,16 @@ document.addEventListener('DOMContentLoaded', () => {
     function fiveDay(lat, lon, city) {
         const apiKey = '1d06b7c740fb5f44ff9ef2d948306601';
         const cnt = 6
-        //unable to use 16 day since it is a paid subscription
-        // const urlFive = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=${cnt}&appid=${apiKey}`
-        // console.log(urlFive);
+
         const urlFive = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        console.log(urlFive);
-        const urlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
         
-        fetch(urlCurrent)
+        fetch(urlFive)
             .then(function(resp) {
                 return resp.json();
             })
-            .then(async function(currentData) {    
-                const currentTempF = (currentData.main.temp - 273.15) * 9/5 + 32; // Convert from Kelvin to Fahrenheit
-                const currentIconCode = currentData.weather[0].icon; // Get icon code
-                const currentIconUrl = `http://openweathermap.org/img/wn/${currentIconCode}@2x.png`; // Construct icon URL
+            .then(async function(fiveDayData) {    
+                let cityData = [];
 
-                let cityData = [{
-                    city: city,
-                    date: formatDate(currentData.dt * 1000), // Convert Unix timestamp to date
-                    iconUrl: currentIconUrl,
-                    temp: currentTempF.toFixed(1), // Round to one decimal place
-                    wind: currentData.wind.speed,
-                    humidity: currentData.main.humidity
-                }];
-
-                createCurrentCard(cityData, city);
-
-                const fiveDayDataResp = await fetch(urlFive);
-                const fiveDayData = await fiveDayDataResp.json();
-                console.log(fiveDayData);
                 let dates = [];
                 fiveDayData.list.forEach((item) => {
                     if(!dates.includes(item.dt_txt.split(" ")[0])){
@@ -78,17 +61,18 @@ document.addEventListener('DOMContentLoaded', () => {
                             iconUrl: iconUrl,
                             temp: tempF.toFixed(1), // Round to one decimal place
                             wind: item.wind.speed,
-                            humidity: item.main.humidity
+                            humidity: item.main.humidity,
+                            id: crypto.randomUUID()
                         });
                     }
                 });
-                console.log('City Data', cityData);
                 // Add new data to the beginning of the array using unshift
                 fiveDayArr.unshift(cityData);
 
                 // Save to local storage
                 localStorage.setItem('fiveDayArr', JSON.stringify(fiveDayArr));
-                // console.log(fiveDayArr);
+
+                createCurrentCard(cityData, city);
                 createFutureCards(cityData);
             })
             .catch(function(error) {
@@ -116,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
         futureContainerEl.innerHTML = ''; //clears any previous content
 
         //only render indices 1-5 since index 0 is the current day weather
-        for (let i = 2; i<=5 && i < cityDataArray.length; i++) {
+        for (let i = 1; i<=6 && i < cityDataArray.length; i++) {
             const item = cityDataArray[i];
             const div = document.createElement('div');
             div.classList.add('col-span-2', 'p-2', 'bg-cyan-600', 'rounded-md', 'shadow-md', 'shadow-cyan-500/50', 'mb-2');
@@ -153,6 +137,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //listener for the 'submit' on the city search
     cityEl.addEventListener('submit', handleSubmit)
 
+    cityClicked.
     function renderSavedData() {
         if(fiveDayArr.length > 0) {
             const latestCityData = fiveDayArr[0];
@@ -161,6 +146,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+function init(){
     renderSavedData();
-
+}
+    
+init();
 });
